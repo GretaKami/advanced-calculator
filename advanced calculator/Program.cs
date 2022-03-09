cc87a6d503ce7c773d73b2edf347ae90d040c7bb
Greta Kaminskaite <greta.kam@gmail.com>
2022-03-09 22:35:45 +0200

    added number check

--- advanced calculator/Program.cs
+++ advanced calculator/Program.cs
@@ -7,40 +7,47 @@ string Number2 = Console.ReadLine();
 Console.Write("Please enter operation sign: ");
 string Operation = Console.ReadLine();
 
-double.TryParse(Number1, out double number1);
-double.TryParse(Number2, out double number2);
-char.TryParse(Operation, out char operation);
+bool correctNumber1 = double.TryParse(Number1, out double number1);
+bool correctnumber2 = double.TryParse(Number2, out double number2);
+bool correctOperation = char.TryParse(Operation, out char operation);
 
-switch (operation)
+if (correctNumber1 && correctnumber2 && correctOperation)
 {
-    case '+':
-        Console.WriteLine($"Result: {number1} + {number2} = {number1 + number2}");
-        break;
-    case '-':
-        Console.WriteLine($"Result: {number1} - {number2} = {number1 - number2}");
-        break;
-    case '/':
-        Console.WriteLine($"Result: {number1} / {number2} = {number1 / number2}");
-        break;
-    case '*':
-        Console.WriteLine($"Result: {number1} * {number2} = {number1 * number2}");
-        break;
-    case '%':
-        Console.WriteLine($"Result: {number1} % {number2} = {number1 % number2}");
-        break;
-    case 'p':
-        Console.WriteLine($"Result: first number is {number1}, second number is {number2}");
-        break;
-    case 'b':
-        if (number1 > number2) {Console.WriteLine($"Result: first number ({number1}) is bigger"); }
-        else { Console.WriteLine($"Result: second number ({number2}) is bigger"); }
-        break;
-    case 's':
-        if (number1 < number2) { Console.WriteLine($"Result: first number ({number1}) is smaller"); }
-        else { Console.WriteLine($"Result: second number ({number2}) is smaller"); }
-        break;
-    default:
-        Console.WriteLine("Incorrect operation");
-        break;
+    switch (operation)
+    {
+        case '+':
+            Console.WriteLine($"Result: {number1} + {number2} = {number1 + number2}");
+            break;
+        case '-':
+            Console.WriteLine($"Result: {number1} - {number2} = {number1 - number2}");
+            break;
+        case '/':
+            Console.WriteLine($"Result: {number1} / {number2} = {number1 / number2}");
+            break;
+        case '*':
+            Console.WriteLine($"Result: {number1} * {number2} = {number1 * number2}");
+            break;
+        case '%':
+            Console.WriteLine($"Result: {number1} % {number2} = {number1 % number2}");
+            break;
+        case 'p':
+            Console.WriteLine($"Result: first number is {number1}, second number is {number2}");
+            break;
+        case 'b':
+            if (number1 > number2) { Console.WriteLine($"Result: first number ({number1}) is bigger"); }
+            else { Console.WriteLine($"Result: second number ({number2}) is bigger"); }
+            break;
+        case 's':
+            if (number1 < number2) { Console.WriteLine($"Result: first number ({number1}) is smaller"); }
+            else { Console.WriteLine($"Result: second number ({number2}) is smaller"); }
+            break;
+        default:
+            Console.WriteLine("Incorrect operation");
+            break;
 
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect numbers or operation");
 }
