c4638dbe03c4e307ca36576f8302a99e2fb70acf
Greta Kaminskaite <greta.kam@gmail.com>
2022-03-09 22:27:42 +0200

    Add project files.

---
+++ advanced calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Please enter first number: ");
+string Number1 = Console.ReadLine();
+Console.Write("Please enter second number: ");
+string Number2 = Console.ReadLine();
+Console.Write("Please enter operation sign: ");
+string Operation = Console.ReadLine();
+
+double.TryParse(Number1, out double number1);
+double.TryParse(Number2, out double number2);
+char.TryParse(Operation, out char operation);
+
+switch (operation)
+{
+    case '+':
+        Console.WriteLine($"Result: {number1} + {number2} = {number1 + number2}");
+        break;
+    case '-':
+        Console.WriteLine($"Result: {number1} - {number2} = {number1 - number2}");
+        break;
+    case '/':
+        Console.WriteLine($"Result: {number1} / {number2} = {number1 / number2}");
+        break;
+    case '*':
+        Console.WriteLine($"Result: {number1} * {number2} = {number1 * number2}");
+        break;
+    case '%':
+        Console.WriteLine($"Result: {number1} % {number2} = {number1 % number2}");
+        break;
+    case 'p':
+        Console.WriteLine($"Result: first number is {number1}, second number is {number2}");
+        break;
+    case 'b':
+        if (number1 > number2) {Console.WriteLine($"Result: first number ({number1}) is bigger"); }
+        else { Console.WriteLine($"Result: second number ({number2}) is bigger"); }
+        break;
+    case 's':
+        if (number1 < number2) { Console.WriteLine($"Result: first number ({number1}) is smaller"); }
+        else { Console.WriteLine($"Result: second number ({number2}) is smaller"); }
+        break;
+    default:
+        Console.WriteLine("Incorrect operation");
+        break;
+
+}
